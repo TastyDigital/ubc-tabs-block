cda6da049ccaeedbd271ced598516ca5bfbc3d9e
Toby Trembath <toby@tastydigital.com>
2023-02-17 12:00:37 +0000

    Updated Wordpress/scripts and removed redundant sass loaders

---
+++ build/block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '525fd58492af04bc9515');
---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '45bf960ce3ba64a014b0');
